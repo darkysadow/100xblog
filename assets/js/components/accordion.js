01822e1c4c4e11dd437cf65667e0c98f12e173aa
darkysadow <oleksandr.poberezhniak@gmail.com>
2024-11-26 18:32:46 +0200

    Light theme

--- assets/js/components/accordion.js
+++ assets/js/components/accordion.js
@@ -1,25 +1,9 @@
 export function initAccordion() {
-    if(document.querySelector('.our-data__item-head')) {
-        document.querySelectorAll('.our-data__item-head').forEach(item => {
+    if(document.querySelector('.accordion__item')) {
+        document.querySelector('.accordion__head').classList.toggle('opened')
+        document.querySelectorAll('.accordion__head').forEach(item => {
             item.addEventListener('click', function() {
-                document.querySelectorAll('.our-data__item-head').forEach(el => {
-                   if(el===item) {
-                    el.classList.toggle('opened')
-                   } else {
-                    el.classList.remove('opened')
-
-                   }
-                })
-
-            })
-        })
-
-    }
-    if(document.querySelector('.single-blog__accordion-item')) {
-        document.querySelector('.single-blog__accordion-head').classList.toggle('opened')
-        document.querySelectorAll('.single-blog__accordion-head').forEach(item => {
-            item.addEventListener('click', function() {
-                document.querySelectorAll('.single-blog__accordion-head').forEach(el => {
+                document.querySelectorAll('.accordion__head').forEach(el => {
                    if(el===item) {
                     el.classList.toggle('opened')
                    } else {
